9dc9b6cd7944d3220961ff1b01ce8ff3224bacce
Moritz Gunz <moritz.gunz@gmail.com>
2017-11-01 20:37:50 +0100

    :sparkles: Implement properties for attributes

--- src/with-props.ts
+++ src/with-props.ts
@@ -1,8 +1,9 @@
 import camelize from 'lodash-es/camelCase';
+import kebapize from 'lodash-es/kebabCase';
 
 import { FitElement, MapDispatchToProps, MapStateToPropsFn } from './connect.js';
 
-export interface AttributeOptions {
+export interface AttributeDescriptors {
     [key: string]: typeof String | typeof Boolean | typeof Number;
 }
 
@@ -10,33 +11,49 @@ export type AttributeValues<A> = {
     [K in keyof A]: boolean | string | number;
 };
 
-export default function withProps<S, P, A extends AttributeOptions>(
+export default function withProps<S, P, A extends AttributeDescriptors>(
     Base: FitElement<S, P, any>,
-    observedAttributes: A
+    attributeDescriptors: A
 ): FitElement<S, P, AttributeValues<A>> {
+    const observedAttrs = Object.keys(attributeDescriptors).map(kebapize);
+
     return class extends Base {
-        private _observedAttributes: A = observedAttributes;
+        private _attributeDescriptors: A = attributeDescriptors;
         private _ownProps: AttributeValues<A> = {} as AttributeValues<A>;
 
-        get observedAttributes(): string[] {
-            return Object.keys(this._observedAttributes);
+        static get observedAttributes(): string[] {
+            return observedAttrs;
+        }
+
+        constructor() {
+            super();
+
+            const obj = {};
+            for (const propName of observedAttrs) {
+                obj[propName] = obj[camelize(propName)] = {
+                    get: () => this._ownProps[propName],
+                    set: val => this._ownProps[propName] = val
+                };
+            }
+
+            Object.defineProperties(this, obj);
         }
 
         attributeChangedCallback(name: string, _: string, newValue: string) {
-            const realName = camelize(name);
-            if (!(realName in this._observedAttributes)) {
+            if (observedAttrs.indexOf(name) === -1) {
                 return;
             }
 
-            const type = this._observedAttributes[realName];
+            const realName = camelize(name);
+            const type = this._attributeDescriptors[realName];
             const value = (type === Boolean)
                 ? (newValue !== null)
-                : this._observedAttributes[realName](newValue);
+                : this._attributeDescriptors[realName](newValue);
 
             if (value === this._ownProps[realName]) {
                 return;
             }
-            
+
             this._ownProps[realName] = value;
             this.enqueueRender();
         }
