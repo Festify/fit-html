954556c22a7e2a87d7feaab0dce4fd7d7590a2dc
Moritz Gunz <moritz.gunz@gmail.com>
2018-02-02 00:42:03 +0100

    :arrow_up: Upgrade to lit-html 0.9.0
    This renders our withExtended-Mixin obsolete for an even smaller
     size 🎉!

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export { default as connect } from './connect';
 export { default as createProvider } from './provider';
-export { default as withExtended } from './with-extended';
 export { default as withProps } from './with-props';
 
 export * from './connect';
